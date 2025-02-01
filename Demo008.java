50d3e834a7920ceaa96bcd10bc82db1c81f5c3ae
Sahithi Pujyam <2311cs010635@mallareddyuniversity.ac.in>
2025-02-01 09:59:28 +0530

    Update Demo008.java

--- Demo008.java
+++ Demo008.java
@@ -6,28 +6,35 @@ public class Demo008 {
         String Username = null;
         String Pswd = null;
         String reply;
+        boolean attemptedLogin = false;
+
         do {
             System.out.println("Welcome to MallaReddy University!");
             System.out.println("Have you visited our site? (yes/no)");
             System.out.print("Reply: ");
             reply = sc.nextLine();
+
             if (reply.equalsIgnoreCase("no")) {
                 System.out.println("Login here");
-                System.out.println("Enter Username: ");
+                System.out.print("Enter Username: ");
                 Username = sc.nextLine();
-                System.out.println("Enter Pswd: ");
+                System.out.print("Enter Pswd: ");
                 Pswd = sc.nextLine();
-                break;
+                attemptedLogin = true;
             } else {
                 System.out.println("You are a valid Customer");
                 break;
             }
-        } while (!"Sahithi".equals(Username) || !"Sahithi@123".equals(Pswd));
-        if ("Sahithi".equals(Username) && "Sahithi@123".equals(Pswd)) {
-            System.out.println("Login successful! Welcome.");
-        } else {
-            System.out.println("Incorrect Username & Password");
+        } while (!attemptedLogin);
+
+        if (attemptedLogin) {
+            if ("Sahithi".equals(Username) && "Sahithi@123".equals(Pswd)) {
+                System.out.println("Login successful! Welcome.");
+            } else {
+                System.out.println("Incorrect Username & Password");
+            }
         }
+
         sc.close();
     }
 }
